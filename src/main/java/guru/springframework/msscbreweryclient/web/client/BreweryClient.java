103c659ad6bd71a3462ef212a9412f1fdefc3fe8
AlexandreBessard <alexandre.bessard@gmail.com>
2021-11-21 17:28:57 -0500

    First commit to initialize project

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+//Inject properties from application.properties with sfg.brewery
+//If not set, fail
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    //Get sfg.brewery.apihost
+    private String apihost;
+    private final RestTemplate restTemplate;
+
+    //Spring inject RestTemplate Builder
+    public BreweryClient(RestTemplateBuilder restTemplate) {
+        this.restTemplate = restTemplate.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid, BeerDto.class);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//Bring standard context
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void setApihost() {
+    }
+}
